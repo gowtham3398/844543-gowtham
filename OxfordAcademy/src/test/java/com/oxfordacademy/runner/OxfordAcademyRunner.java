3f61b6a8d023d3c0f6541c6033b8b4daaa0a5efe
SANJEEV <SANJEEV@SANJEEV-PC>
2020-03-24 22:43:11 +0530

    fina

--- OxfordAcademy/src/test/java/com/oxfordacademy/runner/OxfordAcademyRunner.java
+++ OxfordAcademy/src/test/java/com/oxfordacademy/runner/OxfordAcademyRunner.java
@@ -1,5 +1,8 @@
 package com.oxfordacademy.runner;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -7,12 +10,16 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/main/resources/Feature/OxfordAcademy.Feature",
-		plugin = {"pretty", "html:reports/cucumber-html-report"},
-				
+		plugin = { "pretty","html:reports/cucumber-html-report","json:reports/cucumber-json-report/jsonreport","com.cucumber.listener.ExtentCucumberFormatter:reports/cucumber-extent-report/extentreport.html" },
 		glue = {"com.oxfordacademy.stepsdefinition"},
 		monochrome = true
 		)
 
 public class OxfordAcademyRunner {
+
+	@AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig("src/test/resources/extent-config.xml");
+    }
 
 }
